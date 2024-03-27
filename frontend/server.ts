b42c5e25b8696f06615e2db6d1e18f9c14470d6b
Marvin Scham <dev@marvinscham.de>
2024-03-27 22:38:03 +0100

    Switched operator

--- frontend/server.ts
+++ frontend/server.ts
@@ -20,9 +20,12 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    '*.*',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+    })
+  );
 
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
@@ -44,7 +47,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] ?? 4000;
 
   // Start up the Node server
   const server = app();
